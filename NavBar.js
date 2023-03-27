9fa78578ee405e3b522bd7892ac7f586394539ed
shvkateryna <116515521+shvkateryna@users.noreply.github.com>
2023-03-27 20:55:19 +0300

    Update NavBar.js

--- NavBar.js
+++ NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useAuth } from "../contexts/AuthContexts"
+import { useAuth } from "./AuthContexts"
 import { useNavigate } from "react-router-dom";
 const NavBar = () => {
     const { currentUser, role } = useAuth()
